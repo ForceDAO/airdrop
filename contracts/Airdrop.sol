ae2480da4882307d87e16af5a7c05aa8d98a69d4
Noah Marconi <noah.marconi@gmail.com>
2021-01-31 18:49:51 -0500

    draft deadline sol changes

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -2,18 +2,33 @@
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 contract Airdrop {
+    using SafeMath for uint256;
 
-    bytes32 public _rootHash;
-    IERC20 public _token;
+    uint256 private constant THREE_WEEKS_OF_BLOCKS = 135450; // Estimated based on 6450 daily blocks * 7 days * 3 weeks.
+    uint256 private constant THREE_DAYS_OF_BLOCKS = 19350;   // Estimated based on 6450 daily blocks * 3 days.
+
+    bytes32 public immutable _rootHash;
+    IERC20 public immutable _token;
+    uint256 public immutable _blockDeadline;
+    uint256 public immutable _blockReductionsBegin;
 
     mapping (uint256 => uint256) _redeemed;
 
     constructor(IERC20 token, bytes32 rootHash) {
+        
         _token = token;
         _rootHash = rootHash;
+
+        _blockReductionsBegin = block.number
+            .add(THREE_WEEKS_OF_BLOCKS);
+        
+        _blockDeadline = block.number
+            .add(THREE_WEEKS_OF_BLOCKS)
+            .add(THREE_DAYS_OF_BLOCKS);
     }
 
     function redeemed(uint256 index) public view returns (bool) {
@@ -23,6 +38,8 @@ contract Airdrop {
     }
 
     function redeemPackage(uint256 index, address recipient, uint256 amount, bytes32[] memory merkleProof) public {
+
+        require(block.number <= _blockDeadline, "Airdrop: Redemption deadline passed.");
 
         // Make sure this package has not already been claimed (and claim it)
         uint256 redeemedBlock = _redeemed[index / 256];
@@ -47,10 +64,30 @@ contract Airdrop {
         require(node == _rootHash, "Airdrop: Merkle root mismatch");
 
         // Redeem!
+        uint256 sendAmount = amount;
+        if (block.number > _blockReductionsBegin) {
+            sendAmount = reducedAmount(amount);
+        }
+
         require(
-            IERC20(_token).transfer(recipient, amount),
+            IERC20(_token).transfer(recipient, sendAmount),
             "Airdrop: Token transfer fail"
         );
 
+    }
+
+    function reducedAmount(uint256 originalAmount)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 blocksSinceReductionStarted = block.number
+            .sub(_blockReductionsBegin);
+
+        uint256 reduceBy = blocksSinceReductionStarted
+            .mul(originalAmount)
+            .div(THREE_DAYS_OF_BLOCKS);
+
+        return originalAmount.sub(reduceBy);
     }
 }
