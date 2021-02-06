596f5795f36344e32b9fa559449bc96af529a7a4
JesseAbram <jesseabramowitz@hotmail.com>
2021-02-06 21:34:59 +0100

    s3 upload

--- merkle/s3Upload.ts
+++ merkle/s3Upload.ts
@@ -1,8 +1,8 @@
 require("dotenv").config();
-import AWS from "aws-sdk";
-const data = require("./data");
+const S3 = require("aws-sdk/clients/s3");
+const data = require("./data/allProofs.json");
 
-const s3 = new AWS.S3({
+const s3 = new S3({
   accessKeyId: process.env.aws_access_key_id,
   secretAccessKey: process.env.aws_secret_access_key,
 });
@@ -13,19 +13,19 @@ const bucketName: string = process.env.bucket_name
 
 export const upload = async (data: any) => {
   for (let i = 0; i < data.length; i++) {
-    const params: AWS.S3.Types.PutObjectRequest = {
+    const params: any = {
       Bucket: bucketName.toString(),
       Key: data[i].leaf.address,
       Body: JSON.stringify(data[i]),
       ContentType: "application/json; charset=utf-8",
-      // ACL: 'public-read', TODO decide if this needs to go in
+      ACL: "public-read",
     };
     await new Promise((resolve, reject) => {
       s3.upload(params, (error, data) => {
         if (error) {
           return reject(error);
         }
-
+        console.log(i, "uploaded");
         return resolve(data);
       });
     });
