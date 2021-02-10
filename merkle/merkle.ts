f3f66e019f9d404475000f36ae35be8d7de4b988
JesseAbram <jesseabramowitz@hotmail.com>
2021-02-10 20:19:33 +0100

    optimize tree

--- merkle/merkle.ts
+++ merkle/merkle.ts
@@ -25,34 +25,31 @@ const reduceMerkleBranches = (leaves: string[]) => {
   return output;
 };
 
-const computeMerkleProof = (balances, index: number) => {
-  let leaves = balances//sortAndAddIndex(balances);
-  let hashedLeaves: string[] = leaves.map(hash);
-
+const computeMerkleProof = (wholeTree: any, index: number) => {
+  let hashedLeaves = wholeTree//sortAndAddIndex(balances);
   if (index == null) {
     throw new Error("address not found");
   }
-
   let path: number = index;
   let proof = [];
-
-  while (hashedLeaves.length > 1) {
+  let i = 0
+  while (i < hashedLeaves.length) {
     if (path % 2 == 1) {
-      proof.push(hashedLeaves[path - 1]);
+      proof.push(hashedLeaves[i][path - 1]);
     } else {
-      if (hashedLeaves[path + 1]) {
-        proof.push(hashedLeaves[path + 1]);
+      if (hashedLeaves[i][path + 1]) {
+        proof.push(hashedLeaves[i][path + 1]);
       } else {
-        proof.push(hashedLeaves[path])
+        proof.push(hashedLeaves[i][path])
       }
     }
 
-    // Reduce the merkle tree one level
-    hashedLeaves = reduceMerkleBranches(hashedLeaves);
-
     // Move up
     path = parseInt((path / 2).toString());
+    i += 1
   }
+    console.log(proof)
+    proof.pop()
 
   return proof;
 };
@@ -60,27 +57,30 @@ const computeMerkleProof = (balances, index: number) => {
 const computeRoot = (balances) => {
   const leaves = balances//sortAndAddIndex(balances);
   let hashedLeaves: string[] = leaves.map(hash);
-
+  let wholeTree = []
+  wholeTree.push(Array.from(hashedLeaves))
   while (hashedLeaves.length > 1) {
     hashedLeaves = reduceMerkleBranches(hashedLeaves);
+      wholeTree.push(Array.from(hashedLeaves))
   }
 
-  return hashedLeaves[0];
+  return {root: hashedLeaves[hashedLeaves.length - 1], wholeTree};
 };
 
 const computeAllProofs = (balances) => {
   const leaves = balances//sortAndAddIndex(balances);
   let proofs = [];
+  const wholeTree = computeRoot(data)
+  console.log(wholeTree)
+  writeToFileSystem(JSON.stringify(wholeTree.root), "rootHash");
   leaves.forEach((leaf, i) => {
     console.time("proof")
-    const proof = computeMerkleProof(leaves, leaf.index);
+    const proof = computeMerkleProof(wholeTree.wholeTree, leaf.index);
     proofs.push({ proof, leaf });
     console.log("proof x generated", i)
     console.timeEnd("proof")
-    // writeToFileSystem(JSON.stringify(proofs[leaf.address]), leaf.address);
   });
   writeToFileSystem(JSON.stringify(proofs), "allProofs");
 };
 
-writeToFileSystem(JSON.stringify(computeRoot(data)), "rootHash");
 computeAllProofs(data);
